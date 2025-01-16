6e8b68ef4c9c99fc8f66f29d309e4ee175e8c3ac
rubentashchyan <gotrek0112@gmail.com>
2025-01-16 19:57:14 +0300

    custom.Array

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.Stack;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+
+
+
+        int a = 1234;
+                MyArrayList<Integer> list = new MyArrayList<>();
+       list.add(1);
+       list.add(2);
+       list.add(3);
+       list.add(4);
+       list.add(5);
+       list.add(6);
+       list.add(7);
+       list.add(8);
+       list.add(9);
+       list.add(10);
+       list.add(11);
+        System.out.println(list.get(10));
+    }
+}
